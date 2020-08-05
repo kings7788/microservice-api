5cdfdaf6c2d34df1e6b9f9caeb826b40e979c3f9
bryant <bryant.chang@velocitylight.net>
2020-08-05 18:19:34 +0800

    增加 部門服務提供者 & 增加 8001 spring boot 啟動代碼

---
+++ microservicecloud-provider-dept-8001/src/main/java/com/bryant/springcloud/DeptProvider8001App.java
@@ -0,0 +1,17 @@
+package com.bryant.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ * @author ARUSER
+ */
+@SpringBootApplication
+public class DeptProvider8001App 
+{
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProvider8001App.class, args);
+    }
+}
---
+++ microservicecloud-provider-dept-8001/src/main/java/com/bryant/springcloud/service/impl/DeptServiceImpl.java
@@ -0,0 +1,31 @@
+package com.bryant.springcloud.service.impl;
+
+import com.bryant.springcloud.dao.DeptDao;
+import com.bryant.springcloud.entity.Dept;
+import com.bryant.springcloud.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DeptServiceImpl implements DeptService {
+
+    @Autowired
+    DeptDao deptDao;
+
+    @Override
+    public boolean addDept(Dept dept) {
+        return deptDao.addDept(dept);
+    }
+
+    @Override
+    public Dept findById(Long id) {
+        return deptDao.findById(id);
+    }
+
+    @Override
+    public List<Dept> list() {
+        return deptDao.findAll();
+    }
+}
