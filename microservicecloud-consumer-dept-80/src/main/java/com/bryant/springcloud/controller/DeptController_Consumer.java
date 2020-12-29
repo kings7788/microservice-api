e537660f2095fca4cdeee3402f25bf20bd9110fd
bryant <bryant.chang@velocitylight.net>
2020-12-29 14:23:38 +0800

    Add DeptController

--- microservicecloud-consumer-dept-80/src/main/java/com/bryant/springcloud/controller/DeptController_Consumer.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/bryant/springcloud/controller/DeptController_Consumer.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class DeptController_Consumer
 {
     private static final String REST_URL_PREFIX = "http://localhost:8001";
+    private static final String REST_URL_HAIFENG = "http://156.236.72.229:3020/api/pay/create_order";
     @Autowired
     private RestTemplate restTemplate;
 
@@ -32,5 +33,9 @@ public class DeptController_Consumer
     @RequestMapping(value = "/consumer/dept/list")
     public List<Dept> list(){
         return restTemplate.getForObject(REST_URL_PREFIX+"/dept/list", List.class);
+    }
+    @RequestMapping(value = "/test/xxx")
+    public String test(){
+        return restTemplate.getForObject(REST_URL_HAIFENG+"?params="+"%7B%22amount%22%3A%2210000%22%2C%22mchId%22%3A%222066693%22%2C%22productId%22%3A%228007%22%2C%22mchOrderNo%22%3A%2220200813142240635979%22%2C%22subject%22%3A%2220200813142240635979%22%2C%22clientIp%22%3A%22121.76.159.174%22%2C%22sign%22%3A%2235830F1CBA183DFDD7BF8C4935757B6D%22%2C%22notifyUrl%22%3A%22http%3A%2F%2F66p.badej8888.com%3A30000%2FrespPayWeb%2FHAIFENGZHIFU_BANK_WAP_ZFB_SM%2F%22%2C%22currency%22%3A%22cny%22%2C%22body%22%3A%2220200813142240635979%22%7D", String.class);
     }
 }
